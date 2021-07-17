9a2d7547792963d7c10fbb17cdf437bda17fe85d
Leonardo Lopes da Luz <leonardolluzti@gmail.com>
2021-07-17 17:04:01 -0300

    Not-Found Criado e Responsivo.

--- src/app/sharedComponents/navbar/navbar.component.ts
+++ src/app/sharedComponents/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.putEvents();
@@ -20,7 +21,10 @@ export class NavbarComponent implements OnInit {
     botaoMenuMobile.addEventListener('click', function(){
       menuList.classList.toggle('active');
     });
-    //mobile.classList.toggle('active');
+    
   }
-
+  logout(){
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 }
